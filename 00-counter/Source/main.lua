901182edf90fcf3cd0f7a8e02284fe094c3110ea
Anthony Miyaguchi <acmiyaguchi@gmail.com>
2025-06-23 00:07:54 -0700

    Count using clockwise crank

--- 00-counter/Source/main.lua
+++ 00-counter/Source/main.lua
@@ -2,8 +2,31 @@ local pd <const> = playdate
 local gfx <const> = playdate.graphics
 
 local counter = 0
+local lastCrank = pd.getCrankPosition()
+local crankAccum = 0
 
 function pd.update()
+    local currentCrank = pd.getCrankPosition()
+    local delta = currentCrank - lastCrank
+
+    -- Handle wrap-around
+    if delta > 180 then
+        delta = delta - 360
+    elseif delta < -180 then
+        delta = delta + 360
+    end
+
+    crankAccum = crankAccum + delta
+    lastCrank = currentCrank
+
+    if crankAccum >= 360 then
+        local steps = math.floor(crankAccum / 360)
+        counter = counter + steps
+        crankAccum = crankAccum - steps * 360
+    elseif crankAccum < 0 then
+        crankAccum = 0
+    end
+
     gfx.clear()
     gfx.drawText("Counter: " .. tostring(counter), 100, 120)
 end
