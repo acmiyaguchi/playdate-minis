fb703d1122cd0f4c804cde28613ed310a42cf905
Anthony Miyaguchi <acmiyaguchi@gmail.com>
2025-06-25 00:08:30 -0700

    Add a tiny menu application

---
+++ 03-menu/Source/main.lua
@@ -0,0 +1,48 @@
+import "CoreLibs/graphics"
+import "../toyboxes/toyboxes.lua"
+
+local gfx <const> = playdate.graphics
+
+local function createMenu(ui)
+    local box = ui.box
+    local text = ui.text
+
+    local n = 8;
+
+    local container = box()
+    for i = 1, n do
+        local row = box({ direction = playout.kDirectionHorizontal })
+        for j = 1, n do
+            k = (i - 1) * n + j
+            local bgColor = gfx.kColorBlack
+            local fgColor = gfx.kColorWhite
+            if (i + j) % 2 == 0 then
+                bgColor = gfx.kColorWhite
+                fgColor = gfx.kColorBlack
+            end
+            local col = box({ border = 1, minHeight = 25, minWidth = 25, backgroundColor = bgColor })
+            col:appendChild(text("" .. k, { color = fgColor }))
+            row:appendChild(col)
+        end
+        container:appendChild(row)
+    end
+    return container
+end
+
+function setup()
+    local menu = playout.tree:build(createMenu)
+    local menuSprite = menu:asSprite()
+    menuSprite:moveTo(
+        playdate.display.getWidth() / 2,
+        playdate.display.getHeight() / 2
+    )
+    menuSprite:add()
+end
+
+-- frame callback
+function playdate.update()
+    gfx.sprite.update()
+    playdate.drawFPS()
+end
+
+setup()
